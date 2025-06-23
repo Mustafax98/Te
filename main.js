8f96cdb709c0277b7877fe1401b052a8c8ee38c5
OnAIr <mm6296225@gmail.com>
2025-06-23 05:43:20 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function logout() {
-  localStorage.removeItem("loggedUser");
-  window.location.href = "/login.html"; // غير المسار حسب مكان صفحة تسجيل الدخول
-}
-
-function goBack() {
-  window.history.back();
-}
